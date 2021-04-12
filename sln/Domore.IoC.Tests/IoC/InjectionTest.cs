668da599637ff9e5a615e215a695afce884cfcd0
Ken Yourek <ken@yourek.net>
2021-04-12 08:21:48 -0500

    Add tests.

--- sln/Domore.IoC.Tests/IoC/InjectionTest.cs
+++ sln/Domore.IoC.Tests/IoC/InjectionTest.cs
@@ -4,15 +4,31 @@ namespace Domore.IoC {
     [TestFixture]
     public class InjectionTest {
         private class InvisibleType {
+            public class Controller {
+            }
         }
 
         [Test]
         public void Into_UsesTypeName() {
-            var obj1 = new object();
-            var subject = new Injection<object>(() => obj1);
+            var obj1 = new InvisibleType.Controller();
+            var subject = new Injection<InvisibleType.Controller>(() => obj1);
             subject.Into("InvisibleType");
-            var obj2 = Injection<object>.For(new InvisibleType());
+            var obj2 = Injection<InvisibleType.Controller>.For(new InvisibleType());
             Assert.That(obj2, Is.SameAs(obj1));
+        }
+
+        private class Dependent {
+            public ControllerClass Controller => Injection<ControllerClass>.For(this);
+            public class ControllerClass {
+            }
+        }
+
+        [Test]
+        public void Into_UsesDerivedType() {
+            var expected = new Dependent.ControllerClass();
+            Inject.This(expected).Into(typeof(object));
+            var actual = new Dependent().Controller;
+            Assert.That(actual, Is.SameAs(expected));
         }
     }
 }
